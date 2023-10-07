4d570051a8f161bfb189ec5f570dfabb81e23456
renan-cavalcante <renancggama@gmail.com>
2023-10-07 20:54:50 -0300

    first commit

---
+++ ex00/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio 0</title>
+</head>
+<body>
+    <?php
+        echo "Olá mundo!!! \u{1F30E}";
+    ?>
+</body>
+</html>
---
+++ ex01/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=ph, initial-scale=1.0">
+    <title>Exercico 1</title>
+</head>
+<body>
+    <?php 
+    phpinfo();
+    ?>
+</body>
+</html>
