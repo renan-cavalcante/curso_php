9bcfe8ad2f1754f7567a2a2e63ec0b373169f5b4
renan-cavalcante <renancggama@gmail.com>
2023-10-08 15:17:57 -0300

    exercicio 2

---
+++ ex02/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    date_default_timezone_set("America/Sao_Paulo");
+        echo "Hoje é dia ". date("d/M/Y");
+        echo " e a hora é " . date("G:i:s T");
+    ?>
+</body>
+</html>
